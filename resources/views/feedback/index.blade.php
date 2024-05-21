37fc994cd9a8b37e4f3116c9fe14a51a913c8837
Kamil Wojtalak <kamilwojtalak99@gmail.com>
2024-05-21 07:54:59 +0200

    add feedback form styling

--- resources/views/feedback/index.blade.php
+++ resources/views/feedback/index.blade.php
@@ -4,11 +4,17 @@
     </x-slot>
 
     <section class="public-section">
-        <h1>{{ __('Send me your feedback!') }}</h1>
-        <form action="{{ route('feedback.store') }}" method="post" class="feedback-form">
-            @csrf
-            <textarea name="content" id="" cols="30" rows="10" placeholder="{{ __('If you want me to reply you back, include your email inside the content.') }}"></textarea>
-            <button>Submit</button>
-        </form>
+        <div class="container">
+            <div class="title-container mb-10">
+                <h1>{{ __('Send me your feedback!') }}</h1>
+            </div>
+            <form action="{{ route('feedback.store') }}" method="post" class="feedback-form mb-20">
+                @csrf
+                <textarea name="content" id="" cols="30" rows="10"
+                    placeholder="{{ __('If you want me to reply you back, include your email inside the content.') }}"></textarea>
+                <button>Submit</button>
+            </form>
+        </div>
     </section>
+
 </x-public-layout>
