cc074c6700a269b99cde55a674067c6a5af78171
Kamil Wojtalak <kamilwojtalak99@gmail.com>
2024-05-21 12:15:08 +0200

    add error flash

--- resources/views/components/public/flash.blade.php
+++ resources/views/components/public/flash.blade.php
@@ -3,3 +3,13 @@
         {{ session('success') }}
     </div>
 @endif
+
+@if ($errors->any())
+    <div class="flash flash-error">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
