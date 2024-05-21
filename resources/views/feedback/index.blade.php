8462e2fc90bd5cd5f65186c029d308f196c855bd
Kamil Wojtalak <kamilwojtalak99@gmail.com>
2024-05-21 07:45:15 +0200

    add h1 to feedback page

--- resources/views/feedback/index.blade.php
+++ resources/views/feedback/index.blade.php
@@ -4,7 +4,8 @@
     </x-slot>
 
     <section class="public-section">
-        <form action="{{ route('feedback.store') }}" method="post">
+        <h1>{{ __('Send me your feedback!') }}</h1>
+        <form action="{{ route('feedback.store') }}" method="post" class="feedback-form">
             @csrf
             <textarea name="content" id="" cols="30" rows="10" placeholder="{{ __('If you want me to reply you back, include your email inside the content.') }}"></textarea>
             <button>Submit</button>
