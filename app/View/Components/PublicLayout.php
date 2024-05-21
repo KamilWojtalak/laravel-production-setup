0a16ca6a6cb7a6bf17a5ee55b99f839da7161123
Kamil Wojtalak <kamilwojtalak99@gmail.com>
2024-05-21 07:11:14 +0200

    make component work

--- app/View/Components/PublicLayout.php
+++ app/View/Components/PublicLayout.php
@@ -7,9 +7,6 @@ use Illuminate\View\View;
 
 class PublicLayout extends Component
 {
-    /**
-     * Get the view / contents that represents the component.
-     */
     public function render(): View
     {
         return view('layouts.public');
---
+++ resources/views/feedback/index.blade.php
@@ -0,0 +1,5 @@
+{{-- Note --}}
+{{-- Tutaj podajesz nazwe class, którą zarejestrowałeś w App/Components --}}
+<x-public-layout>
+    test
+</x-public-layout>
