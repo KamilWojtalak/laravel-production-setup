158dde5de2e54dded89656d018d13d9008d38e71
Kamil Wojtalak <kamilwojtalak99@gmail.com>
2024-05-23 12:02:55 +0200

    add stripe views

---
+++ resources/views/stripe/cancel.blade.php
@@ -0,0 +1,11 @@
+<x-public-layout>
+    <x-slot name="header">
+        {{ __('CANCEL Stripe') }}
+    </x-slot>
+
+    <section class="public-section">
+        <div class="container">
+            <h2>Cancel page</h2>
+        </div>
+    </section>
+</x-public-layout>
---
+++ resources/views/stripe/success.blade.php
@@ -0,0 +1,11 @@
+<x-public-layout>
+    <x-slot name="header">
+        {{ __('Sukces Stripe') }}
+    </x-slot>
+
+    <section class="public-section">
+        <div class="container">
+            <h2>SUKCES PAGE</h2>
+        </div>
+    </section>
+</x-public-layout>
