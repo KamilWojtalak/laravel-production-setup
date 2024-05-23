5880f45d817b9cc56ba75c41c5861c879b149449
Kamil Wojtalak <kamilwojtalak99@gmail.com>
2024-05-23 17:49:01 +0200

    add user relation to order model and create orders table

---
+++ database/migrations/2024_05_23_154405_create_orders_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->unsignedBigInteger('user_id')->constrained();
+            $table->string('status', 50)->default('cart');
+            $table->decimal('price');
+            $table->string('payment_session_id');
+            $table->string('payment_provider');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
