44298dc4d56cfc53e6fa252da154891658704099
Kamil Wojtalak <kamilwojtalak99@gmail.com>
2024-05-23 15:13:39 +0200

    add csrf exception

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->validateCsrfTokens(except: [
+            'stripe/fallback',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
