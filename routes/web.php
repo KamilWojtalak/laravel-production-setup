999aa80769a0c576184bbe658fb80b5a46fae9a6
Kamil Wojtalak <kamilwojtalak99@gmail.com>
2024-05-23 20:06:30 +0200

    add orders index route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\FeedbackController;
+use App\Http\Controllers\OrdersController;
 use App\Http\Controllers\StripeController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,5 +17,8 @@ Route::post('/stripe', [StripeController::class, 'store'])->name('stripe.store')
 Route::post('/stripe/fallback', [StripeController::class, 'fallback'])->name('stripe.fallback');
 Route::get('/stripe/success', [StripeController::class, 'success'])->name('stripe.success');
 Route::get('/stripe/cancel', [StripeController::class, 'cancel'])->name('stripe.cancel');
+
+Route::get('/orders', [OrdersController::class, 'index'])->name('orders.index');
+
 
 require __DIR__.'/auth.php';
