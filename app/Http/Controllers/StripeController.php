9fedf3292ba78ce5e91c8e00238d785a3c4aec16
Kamil Wojtalak <kamilwojtalak99@gmail.com>
2024-05-23 11:25:30 +0200

    add stripe view

--- app/Http/Controllers/StripeController.php
+++ app/Http/Controllers/StripeController.php
@@ -11,7 +11,7 @@ class StripeController extends Controller
      */
     public function index()
     {
-        //
+        return view('stripe.index');
     }
 
     /**
---
+++ resources/views/stripe/index.blade.php
@@ -0,0 +1,11 @@
+<x-public-layout>
+    <x-slot name="header">
+        {{ __('Stripe') }}
+    </x-slot>
+
+    <section class="public-section">
+        <div class="container">
+            <h2>Tutaj będzie coś ze stripe</h2>
+        </div>
+    </section>
+</x-public-layout>
