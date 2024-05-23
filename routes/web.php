53b45be670dc8b65020ab37dac87b8f84fa2e62c
Kamil Wojtalak <kamilwojtalak99@gmail.com>
2024-05-23 11:23:01 +0200

    add stripe route

--- routes/web.php
+++ routes/web.php
@@ -10,4 +10,6 @@ Route::get('/', function () {
 Route::get('/feedback', [FeedbackController::class, 'index'])->name('feedback.index');
 Route::post('/feedback', [FeedbackController::class, 'store'])->name('feedback.store');
 
+Route::get('/stripe', [StripeController::class, 'index'])->name('stripe.index');
+
 require __DIR__.'/auth.php';
