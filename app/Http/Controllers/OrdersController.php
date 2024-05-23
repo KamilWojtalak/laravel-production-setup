b1bac5bd934b1e93ca28df3d0c976c562f93d577
Kamil Wojtalak <kamilwojtalak99@gmail.com>
2024-05-23 20:10:40 +0200

    add orders index view display

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
 use Illuminate\Http\Request;
 
 class OrdersController extends Controller
 {
     public function index()
     {
-        return view('orders.index');
+        $orders = Order::get();
+
+        return view('orders.index', [
+            'orders' => $orders
+        ]);
     }
 }
--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -6,7 +6,13 @@
     <section class="public-section">
         <div class="container">
             <p>orders</p>
-            <p>...</p>
+            @forelse ($orders as $order)
+                <li>
+                    {{ $order->id }} | {{ $order->status }} | {{ $order->payment_provider }} || {{ $order->price }}
+                </li>
+            @empty
+                <p>Brak zamówień</p>
+            @endforelse
         </div>
     </section>
 </x-public-layout>
