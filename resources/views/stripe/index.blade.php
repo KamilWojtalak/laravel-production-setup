c070251ae834f72f8431c07fd3ceba4907f6cb9c
Kamil Wojtalak <kamilwojtalak99@gmail.com>
2024-05-23 12:05:48 +0200

    add checkout form

--- resources/views/stripe/index.blade.php
+++ resources/views/stripe/index.blade.php
@@ -6,6 +6,20 @@
     <section class="public-section">
         <div class="container">
             <h2>Tutaj będzie coś ze stripe</h2>
+
+            {{-- To ze stripe jest --}}
+            <div class="product">
+                <img src="https://i.imgur.com/EHyR2nP.png" alt="The cover of Stubborn Attachments" />
+                <div class="description">
+                    <h3>Nazwa produktu</h3>
+                    <h5>cena: $20.00</h5>
+                </div>
+            </div>
+
+            <form action="{{ route('stripe.store') }}" method="POST">
+                @csrf
+                <button type="submit" id="checkout-button">Checkout</button>
+            </form>
         </div>
     </section>
 </x-public-layout>
