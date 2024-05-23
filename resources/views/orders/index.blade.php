062d96e6a6a0dc8125111003ca7b3b4fcd60acd6
Kamil Wojtalak <kamilwojtalak99@gmail.com>
2024-05-23 20:07:06 +0200

    add orders view

---
+++ resources/views/orders/index.blade.php
@@ -0,0 +1,12 @@
+<x-public-layout>
+    <x-slot name="header">
+        {{ __('Orders') }}
+    </x-slot>
+
+    <section class="public-section">
+        <div class="container">
+            <p>orders</p>
+            <p>...</p>
+        </div>
+    </section>
+</x-public-layout>
