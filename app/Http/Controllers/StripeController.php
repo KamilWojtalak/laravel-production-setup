7cdd8dad5f2ba4f348061fd25dcc6dfe9f572bf6
Kamil Wojtalak <kamilwojtalak99@gmail.com>
2024-05-23 12:03:16 +0200

    add stripe checkout

--- app/Http/Controllers/StripeController.php
+++ app/Http/Controllers/StripeController.php
@@ -2,61 +2,54 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\Payments\StripeService;
 use Illuminate\Http\Request;
 
 class StripeController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         return view('stripe.index');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+        $stripe = new StripeService();
+
+        $stripe->setApiKey();
+
+        $stripe->createCheckoutSession();
+
+        $redirectUrl = $stripe->getRedirectUrl();
+
+        return redirect()->away($redirectUrl);
     }
 
-    /**
-     * Display the specified resource.
-     */
+    public function success()
+    {
+        return view('stripe.success');
+    }
+
+    public function cancel()
+    {
+        return view('stripe.cancel');
+    }
+
     public function show(string $id)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(string $id)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, string $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
         //
