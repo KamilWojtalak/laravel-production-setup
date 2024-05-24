fe3a4a7c72ead6cfb1ad63d2a9adf144131cb864
Kamil Wojtalak <kamilwojtalak99@gmail.com>
2024-05-24 06:13:42 +0200

    modify dashboard name link

--- routes/auth.php
+++ routes/auth.php
@@ -13,7 +13,7 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard.index');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
