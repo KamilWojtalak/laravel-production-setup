bb068820c32d41e950da2c60151d6a70e791959f
Kamil Wojtalak <kamilwojtalak99@gmail.com>
2024-05-24 12:11:54 +0200

    cs

--- database/migrations/2024_05_24_100945_drop_plan_users_table.php
+++ database/migrations/2024_05_24_100945_drop_plan_users_table.php
@@ -26,5 +26,6 @@ return new class extends Migration
             $table->unsignedBigInteger('user_id')->constrained();
             $table->unsignedBigInteger('plan_id')->constrained();
             $table->timestamp('plan_payed_at');
-        });    }
+        });
+    }
 };
