3f9077a794286d911eefa7707e4bf50e86414d20
Kamil Wojtalak <kamilwojtalak99@gmail.com>
2024-05-24 12:10:43 +0200

    drop plan_users table

---
+++ database/migrations/2024_05_24_100945_drop_plan_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::dropIfExists('plan_users');
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::create('plan_users', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->unsignedBigInteger('user_id')->constrained();
+            $table->unsignedBigInteger('plan_id')->constrained();
+            $table->timestamp('plan_payed_at');
+        });    }
+};
