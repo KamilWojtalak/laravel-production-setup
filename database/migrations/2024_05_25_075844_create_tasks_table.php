b9d0c03d75708407c47471391fca551cbbf24f4a
Kamil Wojtalak <kamilwojtalak99@gmail.com>
2024-05-25 10:31:12 +0200

    define tasks migraiton

--- database/migrations/2024_05_25_075844_create_tasks_table.php
+++ database/migrations/2024_05_25_075844_create_tasks_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->unsignedBigInteger('user_id');
+            $table->string('name');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
