67f1a25c75616da86b8dc924b7182d942fe98f2f
Kamil Wojtalak <kamilwojtalak99@gmail.com>
2024-05-25 11:37:58 +0200

    modify success flash color

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,7 +25,7 @@
         @endif
 
         @if (session('success'))
-            <p style="color: red">{{ session('success') }}</p>
+            <p style="color: green">{{ session('success') }}</p>
         @endif
 
         @if (Auth::user()->doShowPlanPaymentRemainder())
