0b00643269dbdd890a2d58c23068c0d0fddad27e
Kamil Wojtalak <kamilwojtalak99@gmail.com>
2024-05-25 04:41:31 +0200

    modify stripe view, make it dynamic

--- resources/views/stripe/index.blade.php
+++ resources/views/stripe/index.blade.php
@@ -5,33 +5,25 @@
 
     <section class="public-section">
         <div class="container">
+
             <div class="flex">
-                {{-- To ze stripe jest --}}
-                <div class="product">
-                    <img src="https://i.imgur.com/EHyR2nP.png" alt="The cover of Stubborn Attachments" width="450" />
-                    <div class="description">
-                        <h3>Plan One</h3>
-                        <h5>cena: $2.00</h5>
+                @forelse ($plans as $plan)
+                    {{-- To ze stripe jest --}}
+                    <div class="product">
+                        <img src="https://place-hold.it/450" alt="The cover of Stubborn Attachments" />
+                        <div class="description">
+                            <h3>{{ $plan->name }}</h3>
+                            <h5>cena: $3.00</h5>
+                        </div>
+                        <form action="{{ route('stripe.store') }}" method="POST">
+                            @csrf
+                            <input type="hidden" name="plan" value="{{ $plan->name }}">
+                            <button type="submit" id="checkout-button">Checkout Plan {{ $plan->name }}</button>
+                        </form>
                     </div>
-                    <form action="{{ route('stripe.store') }}" method="POST">
-                        @csrf
-                        <input type="hidden" name="plan" value="one">
-                        <button type="submit" id="checkout-button">Checkout Plan One</button>
-                    </form>
-                </div>
-                {{-- To ze stripe jest --}}
-                <div class="product">
-                    <img src="https://place-hold.it/450" alt="The cover of Stubborn Attachments" />
-                    <div class="description">
-                        <h3>Plan Two</h3>
-                        <h5>cena: $3.00</h5>
-                    </div>
-                    <form action="{{ route('stripe.store') }}" method="POST">
-                        @csrf
-                        <input type="hidden" name="plan" value="two">
-                        <button type="submit" id="checkout-button">Checkout Plan Two</button>
-                    </form>
-                </div>
+                @empty
+                    <div class="">Brak Planów w bazie</div>
+                @endforelse
             </div>
 
         </div>
