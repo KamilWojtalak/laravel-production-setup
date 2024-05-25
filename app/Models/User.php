7ac89b66e7a0cb386cda27d963968ea6f6719487
Kamil Wojtalak <kamilwojtalak99@gmail.com>
2024-05-25 12:50:53 +0200

    add authorization methods to user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,7 @@ use App\Observers\UserObserver;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -64,6 +65,11 @@ class User extends Authenticatable
     {
         return $this->belongsToMany(Plan::class, 'orders', 'user_id', 'plan_id', 'id', 'id')
             ->withPivot('payed_at');
+    }
+
+    public function tasks(): HasMany
+    {
+        return $this->hasMany(Task::class);
     }
 
     public function getPlan(): ?Plan
@@ -148,4 +154,43 @@ class User extends Authenticatable
 
         return $this->hasPayedForPlanBetween($start, $end);
     }
+
+    public function canCreateTask(): bool
+    {
+        $tasksCount = $this->tasks()->count();
+        $plan = $this->getPlan();
+
+        if ($this->hasNotPlanAndHasLessThanFreePlanMaxTasks($plan, $tasksCount))
+        {
+            return true;
+        }
+
+        if ($this->hasFirstPlanAndHasLessThanFirstPlanMaxTasks($plan, $tasksCount))
+        {
+            return true;
+        }
+
+        if ($this->hasSecondPlan($plan))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    private function hasNotPlanAndHasLessThanOneTask(?Plan $plan, int $tasksCount): bool
+    {
+        return is_null($plan) && $tasksCount < Plan::MAX_TASKS_PER_PLAN_FREE;
+    }
+
+    private function hasFirstPlanAndHasLessThanFirstPlanMaxTasks(?Plan $plan, int $tasksCount): bool
+    {
+        return $plan->name === PLAN::TYPE_FIRST && $tasksCount < PLAN::MAX_TASKS_PER_PLAN_FIRST;
+    }
+
+    private function hasSecondPlan(?Plan $plan): bool
+    {
+        return $plan->name === PLAN::TYPE_SECOND;
+    }
+
 }
