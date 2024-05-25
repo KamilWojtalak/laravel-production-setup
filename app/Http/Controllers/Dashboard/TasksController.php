fed4df40a89ca830e9d1c8460b5909bca62b07be
Kamil Wojtalak <kamilwojtalak99@gmail.com>
2024-05-25 10:39:36 +0200

    remove unused comments

--- app/Http/Controllers/Dashboard/TasksController.php
+++ app/Http/Controllers/Dashboard/TasksController.php
@@ -7,33 +7,21 @@ use Illuminate\Http\Request;
 
 class TasksController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         return view('dashboard.tasks.index');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
         return view('dashboard.tasks.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
         dd($request->all());
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
         //
