da43f34006d2a7e622ada85212818763de651b6a
Kamil Wojtalak <kamilwojtalak99@gmail.com>
2024-05-25 04:38:55 +0200

    modify stripe view, make it possible to buy second plan

--- resources/views/stripe/index.blade.php
+++ resources/views/stripe/index.blade.php
@@ -5,21 +5,35 @@
 
     <section class="public-section">
         <div class="container">
-            <h2>Tutaj będzie coś ze stripe</h2>
-
-            {{-- To ze stripe jest --}}
-            <div class="product">
-                <img src="https://i.imgur.com/EHyR2nP.png" alt="The cover of Stubborn Attachments" />
-                <div class="description">
-                    <h3>Nazwa produktu</h3>
-                    <h5>cena: $20.00</h5>
+            <div class="flex">
+                {{-- To ze stripe jest --}}
+                <div class="product">
+                    <img src="https://i.imgur.com/EHyR2nP.png" alt="The cover of Stubborn Attachments" width="450" />
+                    <div class="description">
+                        <h3>Plan One</h3>
+                        <h5>cena: $2.00</h5>
+                    </div>
+                    <form action="{{ route('stripe.store') }}" method="POST">
+                        @csrf
+                        <input type="hidden" name="plan" value="one">
+                        <button type="submit" id="checkout-button">Checkout Plan One</button>
+                    </form>
+                </div>
+                {{-- To ze stripe jest --}}
+                <div class="product">
+                    <img src="https://place-hold.it/450" alt="The cover of Stubborn Attachments" />
+                    <div class="description">
+                        <h3>Plan Two</h3>
+                        <h5>cena: $3.00</h5>
+                    </div>
+                    <form action="{{ route('stripe.store') }}" method="POST">
+                        @csrf
+                        <input type="hidden" name="plan" value="two">
+                        <button type="submit" id="checkout-button">Checkout Plan Two</button>
+                    </form>
                 </div>
             </div>
 
-            <form action="{{ route('stripe.store') }}" method="POST">
-                @csrf
-                <button type="submit" id="checkout-button">Checkout</button>
-            </form>
         </div>
     </section>
 </x-public-layout>
